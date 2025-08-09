7374e5df752fc07800f16012ec84a2368fe6f9ab
BobbyLin <linzhangsheng23@gmail.com>
2025-08-09 22:30:19 +0800

    chore: add convex function

--- apps/web/app/page.tsx
+++ apps/web/app/page.tsx
@@ -1,12 +1,21 @@
-import { Button } from "@workspace/ui/components/button"
+'use client'
+
+import { useMutation, useQuery } from 'convex/react'
+
+import { api } from '@workspace/backend/convex/_generated/api'
+import { Button } from '@workspace/ui/components/button'
 
 export default function Page() {
-  return (
-    <div className="flex items-center justify-center min-h-svh">
-      <div className="flex flex-col items-center justify-center gap-4">
-        <h1 className="text-2xl font-bold">Hello World</h1>
-        <Button size="sm">Button</Button>
-      </div>
-    </div>
-  )
+	const users = useQuery(api.user.getMany)
+	const addUser = useMutation(api.user.add)
+
+	return (
+		<div className="flex min-h-svh flex-col items-center justify-center">
+			<p>apps/web</p>
+			<Button onClick={() => addUser()}>Add</Button>
+			<div className="mx-auto w-full max-w-sm">
+				{JSON.stringify(users, null, 2)}
+			</div>
+		</div>
+	)
 }
